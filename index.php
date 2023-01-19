a70456d4abe9c18afb3c57ebaf9cc38f1f58b7d6
DaW-S <david.starek8@gmail.com>
2023-01-19 01:06:05 +0100

    added decrypt

--- index.php
+++ index.php
@@ -2,10 +2,12 @@
 
 $data_json = file_get_contents('data.json');
 $decoded_json = json_decode($data_json, false);
-$input = $decoded_json->input;
-$add = $decoded_json->add;
+$input_encrypt = $decoded_json->input_encrypt;
+$add_encrypt = $decoded_json->add_encrypt;
+$input_decrypt = $decoded_json->input_decrypt;
+$add_decrypt = $decoded_json->add_decrypt;
 
-function caesarChipher (string $input, int $add) {
+function caesarEncrypter (string $input, int $add) {
 
   if ($add > 26) {
     $add = $add % 26;
@@ -42,4 +44,43 @@ function caesarChipher (string $input, int $add) {
   
 }
 
-caesarChipher($input, $add);
+function caesarDecrypter (string $input, int $add) {
+
+  if ($add > 26) {
+    $add = $add % 26;
+  }
+
+  //check forbidden symbols
+  if (preg_replace('/[^a-zA-Z]+/', '_', $input)) { 
+    $strToArr = str_split(strtolower($input));
+    $nextChar = [];
+
+    foreach ($strToArr as $char) {
+      //ascii number
+      $ascii = ord($char);
+
+      if ($ascii == 32 ) {
+        $nextChar[] = ' ';
+        continue;
+      }
+      if (($ascii - $add) < 97) {
+        $nextChar[] = chr($ascii - $add + 26);
+      }
+      else {
+      $nextChar[] = chr(ord($char) - $add);
+      
+      }
+    }
+
+    echo implode($nextChar);
+  }
+
+  else {
+    echo 'Please inser text without symbols.';
+  }
+  
+}
+
+caesarEncrypter($input_encrypt, $add_encrypt);
+echo '<br>';
+caesarDecrypter($input_decrypt, $add_decrypt);
